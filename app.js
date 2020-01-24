3eb4ca660e8d41fe0c6ba2e2ec1c3a1a27671fcd
Maksym Ivanov <ivanovma41@gmail.com>
2020-01-25 01:09:08 +0200

    Set up production settings

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-
+const path = require('path')
 const app = express();
 
 app.use(express.json({extended:true}))
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // если порт не указан в конфиге, то порт будет определен на 5000
 const PORT = config.get('port') || 5000;
